16170a6cdbf1c88ebd521456ea1d25964a837909
nucleonik <77455172+nucleonik@users.noreply.github.com>
2021-07-11 21:36:21 +0600

    Add files via upload

---
+++ catsub.js
@@ -0,0 +1,695 @@
+[
+  {
+    "categoryName": "Grocery and Food",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Grocery.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Baking Needs",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Baking Needs.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Baking and Dessert Mixes",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Baking and dessert mixes.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Baking Ingredients",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Baking Ingredients.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Flour",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Flour.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Nuts and Dried Fruits",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Nuts & Dried Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Beverages",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Beverage.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Coffee",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Coffee.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Juice",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Juice.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Soft Drinks",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Soft Drinks.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Syrups and Powder Drinks",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Soft Drinks.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Tea",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Tea.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Water",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Water.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Bread and Bakery",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Bread & Bakery.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Bread",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Bread.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Cakes",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\cake.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Dips and Spreads",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Dips and spreads.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Honey",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Honey.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Breakfast",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Breakfast.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Cereals",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Cereal.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Energy Booster",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Energy Booster.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Jam and Spreads",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Jam and Spreads.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Local Breakfast",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Local Breakfast.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Cooking",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Cooking.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Dal and Lentil",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\daal.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Ghee",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Ghee.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Oil",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Oil.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Rice",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Rice.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Salt and Sugar",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Sugar.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Shemai and Suji",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\shemai.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Spices",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Spices.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Dairy",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Dairy.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Butter and Sour Cream",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Butter and Sour Cream.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Cheese",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Cheese.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Eggs",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Egg.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Liquid and UHT Milk",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Milk.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Powder Milk and Cream",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Powder Milk.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Yogurt and Sweet",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Yogurt.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Diabetic Food",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Diabetic Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Food Accessories",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Food Accessories.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Frozen and Canned",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Frozen & Canned.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Canned Food",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Canned Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Frozen Snacks",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Frozen Snacks.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Meat and Fish",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Fish & Meat.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Dried Fish",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Dried Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Fresh Fish",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Fresh Fish.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Meat",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\meat.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Tofu and Meat Alternatives",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Tofu.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Snacks",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Grocery\\Snacks.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Biscuits",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Biscuits.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Candy and Chocolates",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\candy.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Chips and Pretzels",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Chips.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Noodles",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\noodles.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Pasta and Macaroni",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\noodles.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Popcorn and Nuts",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Popcorn.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Salad Dressing",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Salad Dressing.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Sauces",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Sauces.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Soups",
+    "image": "\\Cat\\Sub\\Grocery\\tag\\Soup.png",
+    "": ""
+  },
+  {
+    "categoryName": "Health and Beauty",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Health and Beauty.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Health Care",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Health and Beauty\\Health Care.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Antiseptics",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder\\Antiseptic.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "First Aid",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder\\First Aid.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Food Suppliments",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder\\Food Suppliment.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Herbals and Digestive Aid",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder\\Herbal.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Personal Care",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Health and Beauty\\Personal Care.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Bath",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\Bath.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Beard Grooming",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\Beard.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Cream and Lotion",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\cream.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Deodorants",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\Deodorant.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Facial Care",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\facewash.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Hair Care",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\hair cream.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Oral Care",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\oral care.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Shampoo",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\shampoo.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Shaving Needs",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\Shaving cream.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "",
+    "tagName": "Skin Care",
+    "image": "\\Cat\\Sub\\Health and Beauty\\New folder (2)\\Skin care.png",
+    "": ""
+  },
+  {
+    "categoryName": "Home Appliance",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Home Appliance.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Kitchen Appliances",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Appliance\\Utensils.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Light and Electrical",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Appliance\\Light and Electrical.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Tool and Hardware",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Appliance\\Hardware Tools.png",
+    "": ""
+  },
+  {
+    "categoryName": "Home Cleaning",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Home and Cleaning.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Air Freshners",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Cleaning\\AirFreshner.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Cleaning Accessories",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Cleaning\\Cleaning Accessories.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Cleaning Supplies",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Cleaning\\Cleaning Supplies.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Laundry",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Home Cleaning\\Laundry.png",
+    "": ""
+  },
+  {
+    "categoryName": "Regular Products",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Regular Products.png",
+    "": ""
+  },
+  {
+    "categoryName": "Organic Products",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Organic Products.png",
+    "": ""
+  },
+  {
+    "categoryName": "Kid's Zone",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Kids Zone.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Baby Accessories",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Kids Zone\\Baby Accessories.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Bath and Skin Care",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Kids Zone\\Bath & Skin Care.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Diapers and Wipes",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Kids Zone\\Diapers.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "New Born Essentials",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Kids Zone\\New Born Essentials.png",
+    "": ""
+  },
+  {
+    "categoryName": "Medical Device",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Medical Device.png",
+    "": ""
+  },
+  {
+    "categoryName": "Pet Care",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Pet Care.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Cat Food",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Pet Care\\Cat Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Dog Food",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Pet Care\\Dog Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Kitten Food",
+    "tagName": "",
+    "image": "\\Cat\\Sub\\Pet Care\\Kitten Food.png",
+    "": ""
+  },
+  {
+    "categoryName": "Automobile Essential",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Automobile Accessories.png",
+    "": ""
+  },
+  {
+    "categoryName": "Best Offer",
+    "subCategoryName": "",
+    "tagName": "",
+    "image": "\\Cat\\Best Offer.png",
+    "": ""
+  },
+  {
+    "categoryName": "",
+    "subCategoryName": "Buy 1 Get 1",
+    "tagName": "",
+    "image": "\\Cat\\Best Offer.png",
+    "": ""
+  }
+]
