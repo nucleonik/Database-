6d95ca30c3197ea1244411d85724f18cb58b9bde
nucleonik <77455172+nucleonik@users.noreply.github.com>
2021-07-12 21:39:13 +0600

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,46 @@
+[
+  {
+    "category": "Grocery and Food",
+    "image": "/Cat/Grocery.png"
+  },
+  {
+    "category": "Health and Beauty",
+    "image": "/Cat/Health and Beauty.png"
+  },
+  {
+    "category": "Home Appliance",
+    "image": "/Cat/Home Appliance.png"
+  },
+  {
+    "category": "Home Cleaning",
+    "image": "/Cat/Home and Cleaning.png"
+  },
+  {
+    "category": "Regular Products",
+    "image": "/Cat/Regular Products.png"
+  },
+  {
+    "category": "Organic Products",
+    "image": "/Cat/Organic Products.png"
+  },
+  {
+    "category": "Kid's Zone",
+    "image": "/Cat/Kids Zone.png"
+  },
+  {
+    "category": "Medical Device",
+    "image": "/Cat/Medical Device.png"
+  },
+  {
+    "category": "Pet Care",
+    "image": "/Cat/Pet Care.png"
+  },
+  {
+    "category": "Automobile Essential",
+    "image": "/Cat/Automobile Accessories.png"
+  },
+  {
+    "category": "Best Offer",
+    "image": "/Cat/Best Offer.png"
+  }
+]
